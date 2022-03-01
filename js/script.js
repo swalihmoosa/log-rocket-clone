e16f819c16d56b3baec91a5760020c43c713d4ea
swalihmoosa <swalihmoosa72@gmail.com>
2022-03-01 12:29:17 +0530

    Hamburger | ongoing

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 window.addEventListener("load",()=>{
     var icon = document.getElementById("hamburger-icon");
     var menu = document.getElementById("hamburger-menu");
+    var close = document.getElementById("close");
+    var overlay = document.getElementById("overlay");
     
     icon.onclick = () =>{
         menu.classList.add('active')
-
+        overlay.classList.add('active')
+    }
+    close.onclick = () =>{
+        menu.classList.remove('active')
+        overlay.classList.remove('active')
+    }
+    overlay.onclick = () =>{
+        menu.classList.remove('active')
+        overlay.classList.remove('active')
     }
 })
