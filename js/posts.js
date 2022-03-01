77f12167f9603821e3e7b76ef100aa319f4b45a0
swalihmoosa <swalihmoosa72@gmail.com>
2022-03-01 18:18:25 +0530

    posts hamburger | completed

--- js/posts.js
+++ js/posts.js
@@ -1,9 +1,24 @@
 window.addEventListener("load",()=>{
 
     var hamburger_dot = document.getElementById("hamburger-dot");
+    var hamburger_header = document.getElementById("hamburger-header");
+    var hamburger_close = document.getElementById("hamburger-close");
+    var hamburger_overlay = document.getElementById("hamburger-overlay");
 
     hamburger_dot.onclick = () =>{
-        console.log("hamburger_dot");
+        hamburger_header.classList.add("active")
+        hamburger_close.classList.add("active")
+        hamburger_overlay.classList.add("active")
+    }
+    hamburger_close.onclick = () =>{
+        hamburger_header.classList.remove("active")
+        hamburger_close.classList.remove("active")
+        hamburger_overlay.classList.remove("active")
+    }
+    hamburger_overlay.onclick = () =>{
+        hamburger_header.classList.remove("active")
+        hamburger_close.classList.remove("active")
+        hamburger_overlay.classList.remove("active")
     }
     
 })
